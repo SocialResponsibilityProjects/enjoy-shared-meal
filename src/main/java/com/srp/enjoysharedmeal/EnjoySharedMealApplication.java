f13e5f8d62444847dabd3313a8921807d3304ccd
rsakin <ramazansakin63@gmail.com>
2022-09-14 07:30:17 +0300

    project init

---
+++ src/main/java/com/srp/enjoysharedmeal/EnjoySharedMealApplication.java
@@ -0,0 +1,13 @@
+package com.srp.enjoysharedmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoySharedMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoySharedMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srp/enjoysharedmeal/EnjoySharedMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srp.enjoysharedmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoySharedMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
