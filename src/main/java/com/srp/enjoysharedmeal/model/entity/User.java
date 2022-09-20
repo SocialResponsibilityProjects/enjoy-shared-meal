7b5b4747bf6bc2ae925a3f510de5f90c0cf2d2e7
rsakin <ramazansakin63@gmail.com>
2022-09-20 19:51:58 +0300

    user entity class created

---
+++ src/main/java/com/srp/enjoysharedmeal/model/entity/User.java
@@ -0,0 +1,14 @@
+package com.srp.enjoysharedmeal.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class User {
+
+    @Id
+    private Long id;
+
+}
