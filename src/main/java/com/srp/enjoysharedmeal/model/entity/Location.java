85deb907d5eccf95135f1b15a6eb1265de267e4a
orhnch <orhan.cakmak222@gmail.com>
2022-09-25 16:08:02 +0300

    Sharer-flow : Location has been added

---
+++ src/main/java/com/srp/enjoysharedmeal/model/entity/Location.java
@@ -0,0 +1,22 @@
+package com.srp.enjoysharedmeal.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "location")
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double latitude;
+    private double longitude;
+
+}
